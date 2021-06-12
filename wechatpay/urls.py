578ea018846d5f145bad63b7fceac851aa59e8fb
yajiewen <909092921@qq.com>
2021-06-12 10:46:17 +0800

    Add front-end query order status function

--- wechatpay/urls.py
+++ wechatpay/urls.py
@@ -4,6 +4,5 @@ from .views import *
 urlpatterns = [
     path('buycoin/',buycoin), #充值coin
     path('check_pay/',check_pay), #微信那边回调访问地址
-
-
+    path('payoutcome/<str:order_token>/',get_pay_outcome), #前端查询支付结果
 ]
--- wechatpay/views.py
+++ wechatpay/views.py
@@ -1,5 +1,6 @@
+from django.db.models.query import RawQuerySet
 from django.shortcuts import render,HttpResponse
-from django.http import JsonResponse
+from django.http import JsonResponse, response
 from wechatpay import models
 from account import models as amodels
 import requests
@@ -103,11 +104,61 @@ def check_pay(request):
         '''
         检查对应业务数据的状态，判断该通知是否已经处理过，如果没有处理过再进行处理，如果处理过直接返回结果成功。
         '''
-        print('支付成功！')
-        return HttpResponse('SUCCESS')
+        if data_dict['result_code'] == 'SUCCESS':
+            models.Table.objects.filter(
+                order_token = data_dict['out_trade_no'],
+                coin_num = data_dict['total_fee'],
+                ).update(order_status = PAY_SUCCESS)
+            
+            #获取订单信息
+            tab_obj = models.Table.objects.get(                
+                order_token = data_dict['out_trade_no'],
+                coin_num = data_dict['total_fee'],
+                order_status = PAY_SUCCESS)
+
+            usr_info = amodels.Table.objects.get(usr_email = tab_obj.usr_email) #获取用户信息
+
+            paid_coin = tab_obj.coin_num #充值的金额
+            usr_coin = paid_coin + usr_info.usr_coin #计算用户最终coin
+            amodels.Table.objects.filter(usr_email = tab_obj.usr_email).update(usr_coin = usr_coin)
+            print('支付成功！')
+            return HttpResponse('SUCCESS')
+        else:
+            #支付失败删除订单
+            models.Table.objects.filter(
+            order_token = data_dict['out_trade_no'],
+            coin_num = data_dict['total_fee'],
+            ).delete()
+            print('支付失败！')
+            return HttpResponse('FALL')
     else:
         '''
             此处编写支付失败后的业务逻辑
         '''
         print('支付失败！')
         return HttpResponse('failed')
+
+
+"""
+======前端查询支付结果======
+api: https://www.kidtut.net/wechatpay/payoutcome/订单号/
+方法: get
+
+后端返回值:
+{
+    'is_paid':'no',
+}
+
+"""
+def get_pay_outcome(request,order_token):
+    if request.method == 'GET':
+        response_data = {
+            'is_paid':'no',
+        }
+        if models.Table.objects.filter(order_token = order_token,order_status = PAY_SUCCESS).exists():
+            response_data['is_paid'] = 'yes'
+            return JsonResponse(response_data)
+        else:
+            return JsonResponse(response_data)
+    else:
+        return HttpResponse('Bad request',status = 500)
